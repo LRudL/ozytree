1712d91968bb546fd949cefa722d4259a1807c90
LRudL <rudolf.laine@gmail.com>
2021-10-07 23:22:47 +0100

    Switch command-loop to using set-match and integrating with command categories, improve some texts

--- main.rkt
+++ main.rkt
@@ -4,13 +4,16 @@
          racket/list
          racket/string
          racket/match
+         racket/set
          threading
          json
+         "utils.rkt"
          "cli.rkt"
          "setup.rkt"
          "tree.rkt"
          "history-management.rkt"
          "history-loading.rkt"
+         "commands.rkt"
          (for-syntax racket/base
                      racket/list))
 
@@ -20,49 +23,50 @@
 
 (define (command-loop tree commands)
   (let ((cmd (read-line)))
-    (if (equal? cmd "q")
-        'exited
-        (let ((act (interpret-cmd cmd)))
-          (match (action-name act)
-            ('generic-invalid-command-error
-             (begin
-               (displayln "ERROR: something is wrong with your command. Please try again.")
-               (command-loop tree commands)))
-            ('invalid-prefix-error
-             (begin
-               (displayln (format "ERROR: ~a is not a valid command prefix"
-                                (car (tokenise cmd))))
-               (command-loop tree commands)))
-            ('invalid-arity-error
-             (begin
-               (displayln (format "ERROR: wrong number of parameters"))
-               (command-loop tree commands)))
-            ((or 'make 'delete 'move 'set 'mark 'unmark)
-             (begin
-               (displayln (format "Added ~a action to list"
-                                (action-name act)))
-               (command-loop tree
-                             (add-act act commands))))
-            ((or 'reset 'undo)
-             (begin
-               (displayln (format "Changing action list: ~a"
-                                (action-name act)))
-               (command-loop tree
-                             ((action-fn act) commands))))
-            ((or 'view 'preview 'list)
-             (begin
-               (cond ((equal? (action-name act) 'view)
-                      (displayln "Note: the view command excludes uncommitted changes, use the preview command to include those"))
-                     ((equal? (action-name act) 'preview)
-                      (displayln "Note: the preview command includes uncommitted changes, use the view command to view committed changed and commit to commit")))
-               ((action-fn act) tree commands)
-               (command-loop tree commands)))
-            ('commit
-             ((action-fn act) tree commands)
-             (displayln "COMMITTED all actions; no unsaved actions left.")
-             (command-loop (apply-actions-to-tree tree commands)
-                           '()))
-            (_ (raise "Error: something very strange happened in command-loop")))))))
+    (cond ((equal? cmd "q")
+           (displayln "Exited Ozytree."))
+          ((equal? cmd "")
+           (displayln "")
+           (command-loop tree commands))
+          (#t
+           (let ((act (interpret-cmd cmd)))
+             (set-match (action-name act)
+                        ((set 'generic-invalid-command-error)
+                         (begin
+                           (displayln "ERROR: something is wrong with your command")
+                           (displayln "       Type 'help' to see command list.")
+                           (displayln "       Type 'help' followed by a command name to see help for that command.")
+                           (command-loop tree commands)))
+                        (tree-modifying-commands
+                         (begin
+                           (displayln (format "Added ~a action to list"
+                                              (action-name act)))
+                           (command-loop tree
+                                         (add-act act commands))))
+                        (command-list-modifying-commands
+                         (begin
+                           (displayln (format "Changing action list: ~a"
+                                              (action-name act)))
+                           (command-loop tree
+                                         ((action-fn act) commands))))
+                        (viewing-commands
+                         (begin
+                           (cond ((equal? (action-name act) 'view)
+                                  (displayln "(Note: the view command excludes uncommitted changes.)")
+                                  (displayln "(Use the preview command to include uncommitted changes.)"))
+                                 ((equal? (action-name act) 'preview)
+                                  (displayln "(Note: the preview command includes uncommitted changes)")
+                                  (displayln "(Use the view command to view committed changed.)")
+                                  (displayln "(Use the commit command to commit changes.)")))
+                           ((action-fn act) tree commands)
+                           (command-loop tree commands)))
+                        (history-modifying-commands
+                         (begin
+                           ((action-fn act) tree commands)
+                           (displayln "COMMITTED all actions; no unsaved actions left.")
+                           (command-loop (apply-actions-to-tree tree commands)
+                                         '())))
+                        (#t (raise "Error: something very strange happened in command-loop"))))))))
 
 (define (start-command-loop)
   (let ((commits (commits-from-file)))
